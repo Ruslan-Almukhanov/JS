90ae1e04d8e0f1eb1f10e065634467db5000661c
Ruslan <ruslanalmukhanov@gmail.com>
2019-04-11 19:18:57 +0500

    third task

--- main.js
+++ main.js
@@ -1,37 +1,67 @@
 
 "use strict";
 
-let money,
-    first,
-    second,
-    appData,
-    optionalExpenses,
-    income,
-    expenses,
-    savings,
-    oneDay,
-    time;
+let money = +prompt("Ваш бюджет в месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
 
-//  money = prompt("Ваш бюджет в месяц?");
-//  time = prompt("Введите дату в формате YYYY-MM-DD");
-//  console.log(money);
+let appData = {
+    budget: money,
+    expenses: {}, 
+    optionalExpenses: {}, 
+    income: [],
+    savings: false
+};
 
+// for(let i = 0; i < 2; i++) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//         b = prompt("Во сколько обойдется?");
 
-appData = [money, time, expenses, optionalExpenses, income, savings];
+//     if (typeof(a) === 'String' && typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50  ) {
+//         appData.expenses[a] = b;
+//     } else {
+        // num--;
+//     }  
+// };
 
- 
+// second cycle
 
-first = prompt("Введите обязательную статью расходов в этом месяце");
-second = prompt("Во сколько обойдется?");
- expenses: {
-     first : second
- }
- 
- oneDay = first / 30;
- alert(oneDay);
+// let num = 0;
+// while( num < 2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//     b = prompt("Во сколько обойдется?");
 
- console.log(oneDay);
+//     if (typeof(a) === 'String' && typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50  ) {
+//         appData.expenses[a] = b;
+//     } else {
+            // num--;
+//     }
+//     num++;
+// };
 
- 
+// third cycle
+let num = 0;
+do {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    b = prompt("Во сколько обойдется?");
 
+    if (typeof(a) === 'String' && typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50  ) {
+        appData.expenses[a] = b;
+    } else {
+        num--;
+    }
+    num++;
+} while( num < 2 );
+
+appData.moneyPerDay = appData.budget / 30;
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if ( appData.moneyPerDay < 100 ) {
+    console.log("Низкий уровень дохода");
+} else if ( appData.moneyPerDay > 100 && appData.moneyPerDay < 200 ) {
+    console.log("Средний уровень дохода");
+} else if ( appData.moneyPerDay > 200 ) {
+    console.log("Высокий уровень дохода");
+} else {
+    console.log("Произошла ошибка");
+}
  
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let a = [1,2,3],
+    b = [1,2,3];
+ if (a === b) {
+     console.log('True');
+ } else {
+    console.log('False');
+ }
